ff96cbc07b6ce4abc4e6d1c1e7fc44108f80b4aa
Shoaib Ahmed <hussainshoaib46@yahoo.com>
2020-10-11 10:32:12 +0500

    Updates

---
+++ precache-manifest.1633a8c9711a38cef729f93bd6b19909.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a9cc6dbdad569eb5ea5482f632b3921",
+    "url": "/TodoApp-in-React/index.html"
+  },
+  {
+    "revision": "c0192b5ae41d439a1175",
+    "url": "/TodoApp-in-React/static/css/main.a0c2098f.chunk.css"
+  },
+  {
+    "revision": "23294d91dad898c1357e",
+    "url": "/TodoApp-in-React/static/js/2.0122a2ed.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/TodoApp-in-React/static/js/2.0122a2ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0192b5ae41d439a1175",
+    "url": "/TodoApp-in-React/static/js/main.4e261b1e.chunk.js"
+  },
+  {
+    "revision": "7814734b8eec3572f0bc",
+    "url": "/TodoApp-in-React/static/js/runtime-main.cd0cd8fa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TodoApp-in-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
